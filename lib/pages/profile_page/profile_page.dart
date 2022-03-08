f629578d741d2d6f78607c28951f1398de00b15c
momasal <mmsz78@yahoo.com>
2022-03-08 15:15:49 +0330

    first commit

---
+++ lib/pages/profile_page/profile_page.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          splashRadius: 24,
+          icon: const Icon(Icons.chevron_left_rounded),
+        ),
+        title: const Text('Profile'),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.black,
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 16),
+          _buildProfilePic(),
+          const SizedBox(height: 16),
+          const Text(
+            'Mahdi Salmani',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 8),
+          const Text('+98 912 345 6789'),
+          const SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Edit Profile'),
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+            ),
+          ),
+          const SizedBox(height: 16),
+          _buildItems(
+              icon: const Icon(Icons.person), title: const Text('Profile')),
+          _buildItems(
+              icon: const Icon(Icons.notifications),
+              title: const Text('Notifications')),
+          _buildItems(
+              icon: const Icon(Icons.settings), title: const Text('Settings')),
+          _buildItems(
+              icon: const Icon(Icons.logout), title: const Text('Log Out')),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProfilePic() {
+    return Stack(
+      children: [
+        const CircleAvatar(
+          radius: 60,
+          backgroundImage: NetworkImage(
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVLqfekg_kitC_QJ5kgBUTh2tt5EIcxEnQDQ&usqp=CAU'),
+        ),
+        Positioned(
+          bottom: 0,
+          right: 0,
+          child: FloatingActionButton(
+            mini: true,
+            onPressed: () {},
+            child: const Icon(Icons.camera_alt),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildItems({required Widget icon, required Widget title}) {
+    return Card(
+      elevation: 4.0,
+      clipBehavior: Clip.antiAlias,
+      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      child: ListTile(
+        contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        onTap: () {},
+        leading: icon,
+        iconColor: Colors.black,
+        title: title,
+        trailing: const Icon(Icons.chevron_right_rounded),
+      ),
+    );
+  }
+}
